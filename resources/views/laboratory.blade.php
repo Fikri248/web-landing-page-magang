29276858d37fe3a4f68ed8f2d207c48b3ef5a389
Fikri248 <fikriisfahani12@gmail.com>
2026-06-12 14:55:02 +0700

    feat(ui): redesign laboratory and lab pages navigation, sidebar, and content layout

--- resources/views/laboratory.blade.php
+++ resources/views/laboratory.blade.php
@@ -530,6 +530,13 @@
                     </span>
                 </div>
             </a>
+            <a href="https://iebi.rg.telkomuniversity.ac.id/master_data/roadmap" class="lab-sidebar-category">
+                <div class="lab-sidebar-category-box">
+                    <span>
+                        <img class="lab-sidebar-icon" src="{{ asset('img/menu/Icon_Courses.png') }}" alt="">Roadmap
+                    </span>
+                </div>
+            </a>
         </div>
     </div>
 
